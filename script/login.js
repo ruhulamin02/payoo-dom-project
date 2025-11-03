dd072d243cf96b2bc30b6a3fb55774b1e8382c94
ruhulamin02 <mkruhulamin02@gmail.com>
2025-11-03 13:24:11 +0600

    Initial commit: added login page and dashboard DOM project

---
+++ script/login.js
@@ -0,0 +1,21 @@
+document.getElementById("login-btn").addEventListener("click", function(event) {
+
+    event.preventDefault(); 
+    const mobileNumber = document.getElementById("mobile-number").value;
+    const pin = document.getElementById("pin").value;
+    console.log(typeof(pin));
+    
+    if (mobileNumber.length===11) {
+        if (pin==="1234") {
+            window.location.href = "/main.html";
+        } else {
+            alert("Invalid pin");
+            
+        }
+
+    } else {
+        alert("Invalid mobile number");
+    }
+
+    
+})
